a02bf8d0a2018544f536c90e7da5fe80254c09c3
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-01-30 12:41:26 -0300

    feat: finish interfaces

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,42 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { Heading, Text, VStack } from "@gluestack-ui/themed";
+import { ScreenHeader } from "../componets/ScreenHeader";
+import { HistoryCard } from "../componets/HistoryCard";
+import { useState } from "react";
+import { SectionList } from "react-native";
 
 export function History(){
+  const [exercises, setExercises] = useState([
+    {
+      title: "29.01.25",
+      data: ["Puxada Frontal", "Rosca Direta", "Tríceps"]
+    },
+    {
+      title: "28.01.25",
+      data: ["Abdominal", "Agachamento"]
+    },
+  ])
   return (
-    <Center flex={1}>
-      <Text color="$white">History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+      <SectionList 
+        sections={exercises} 
+        keyExtractor={item=>item} 
+        renderItem={()=><HistoryCard/>}
+        renderSectionHeader={({section})=>(
+        <Heading color="$gray200" fontSize="$md" mt="$10" mb="$3" fontFamily="$heading">{section.title}</Heading>
+        )}
+        style={{paddingHorizontal: 32}}
+        contentContainerStyle={
+          exercises.length === 0 && {flex: 1, justifyContent: "center"}
+        }
+        ListEmptyComponent={()=>(
+          <Text color="$gray100" textAlign="center">
+             Não há exercícios registrados ainda. {"\n"} Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+      
+    </VStack>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,26 +3,45 @@ import { HomeHearder } from "../componets/HomeHearder";
 import { Group } from "../componets/Group";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ExerciseCard } from "../componets/ExerciseCard";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 export function Home(){
+  const [exercises, setExercises] = useState<string[]>(["Agachamento","Supino reto", "Supino inclinado","Supino declinado","Supino com halteres","Pernas"]); 
   const [groups, setGroups] = useState<string[]>(["costas", "Bíceps","Tríceps","ombro"]);
   const[groupSelected, setGroupSelected] = useState<string | null>("costas");
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails(){
+    navigation.navigate("exercise")
+  }
+
   return (
     <VStack flex={1}>
       <HomeHearder/>
       <FlatList data={groups} keyExtractor={item =>item} renderItem={({item})=>
-        <Group name={item} isActived={groupSelected === item} onPress={()=>setGroupSelected(item)}/>}
+        <Group name={item} isActived={groupSelected?.toLocaleLowerCase() === item.toLocaleLowerCase()} onPress={()=>setGroupSelected(item)}/>}
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{paddingHorizontal: 32}}
         style={{marginVertical: 40, maxHeight:44, minHeight:44}}
       />
 
-      <VStack px="$8">
+      <VStack px="$8" flex={1}>
         <HStack justifyContent="space-between" mb="$5" alignItems="center">
           <Heading color="$gray200" fontSize="$md" fontFamily="$heading">Exercícios</Heading>
-          <Text color="$gray200" fontSize="$sm" fontFamily="$body">4</Text>
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">{exercises.length}</Text>
         </HStack>
+        <FlatList 
+        data={exercises} 
+        keyExtractor={item =>item} 
+        renderItem={()=> <ExerciseCard onPress={handleOpenExerciseDetails} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 20}}
+        />
+        
       </VStack>
     </VStack>
   )
