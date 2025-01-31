ccef5e2503f302cec2c0831a952900e5627d734e
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-01-30 22:14:53 -0300

    feat: finished photo gallery

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 
 import { StatusBar,View } from 'react-native';
-import {useFonts, Roboto_700Bold, Roboto_400Regular} from '@expo-google-fonts/roboto'
+import "@/global.css";
 import {GluestackUIProvider, Text, Center} from '@gluestack-ui/themed'
+import {useFonts, Roboto_700Bold, Roboto_400Regular} from '@expo-google-fonts/roboto'
 import {config} from './config/gluestack-ui.config'
 import { Loading } from './src/componets/Loading';
 import { Routes } from '@routes/index';
@@ -9,9 +10,9 @@ export default function App() {
   const [fontsLoader]=useFonts({Roboto_700Bold, Roboto_400Regular})
   return (
     <GluestackUIProvider config={config}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoader? <Routes/>: <Loading/>}
-    </GluestackUIProvider>
+    <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+    {fontsLoader? <Routes/>: <Loading/>}
+  </GluestackUIProvider>
   );
 }
 
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,58 @@
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Text, VStack, useToast } from "@gluestack-ui/themed";
 import { ScreenHeader } from "../componets/ScreenHeader";
 import { ScrollView, TouchableOpacity } from "react-native";
 import { UserPhoto } from "../componets/UserPhoto";
 import { Input } from "../componets/Input";
 import { Button } from "../componets/button";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import { useState } from "react";
+import { ToastMessage } from "../componets/ToastMessage";
 
 export function Profile(){
+  const [userPhoto, setUserPhoto] = useState("https://github.com/dhionecastilhobarbosa.png")
+  const toast = useToast()
+  async function handleUserPhotoSelect(){
+    try{
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ['images'],
+        allowsEditing: true,
+        aspect: [4,4],
+        quality: 1
+      })
+
+      if(photoSelected.canceled){
+        return
+      }
+      const photoUri = photoSelected.assets[0].uri
+      
+      if(photoUri){
+        const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {size: number}
+      
+        if(photoInfo.size && photoInfo.size/1024/1024 > 6){
+          return toast.show({
+            placement: "top",
+            render:({id})=>(
+              <ToastMessage id={id} title="Esta imagem e muito grande." description="Escolha uma de até 5MB." action="error" onClose={()=>{toast.close(id)}}/>
+            )
+          })
+        }
+        setUserPhoto(photoUri)
+      }
+    
+    }catch(error){
+      console.log(error)
+    }
+    
+  }
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil"/>
+      
       <ScrollView contentContainerStyle={{padding: 36}}>
         <Center mt="$6" px="$4">
-          <UserPhoto source={{uri:"https://github.com/dhionecastilhobarbosa.png"}} alt="foto do usuário" size="xl"/>
-          <TouchableOpacity>
+          <UserPhoto source={{uri: userPhoto}} alt="foto do usuário" size="xl"/>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color="$green500" fontFamily="$heading" fontSize="$md" mt="$2" mb="$8">Alterar Foto</Text>
           </TouchableOpacity>
           
